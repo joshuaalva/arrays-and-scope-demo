eb87d1684f9821e1daa523ce556e23170ffff540
Joshua Alvarado <joshalva3@gmail.com>
2022-12-22 16:01:48 -0600

    arrays lecture start with notes

---
+++ arrays.js
@@ -0,0 +1,7 @@
+// Arrays 
+
+let names = ['Joshua', 'Liz', 'Mike'];
+console.log(names);
+console.log(typeof names); // returns object ??
+// an array is actually an object of the type array 
+// object means a few different things 
